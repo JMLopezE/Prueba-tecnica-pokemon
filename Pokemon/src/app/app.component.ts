a167ea3d68e309197ef05a747720fa1e7174aa0a
JMLopezE <julylopez02@gmail.com>
2024-02-05 23:41:15 -0500

    Pokemon

--- Pokemon/src/app/app.component.ts
+++ Pokemon/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { PokemonListComponent } from "./component/pokemon-list/pokemon-list.component";
+import { NabVarComponent } from "./component/nav-bar/nab-var.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, PokemonListComponent]
+    imports: [CommonModule, RouterOutlet, PokemonListComponent, NabVarComponent]
 })
 export class AppComponent {
   title = 'Pokemon';
---
+++ Pokemon/src/app/component/nav-bar/nab-var.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nab-var',
+  standalone: true,
+  imports: [],
+  templateUrl: './nab-var.component.html',
+  styleUrl: './nab-var.component.css'
+})
+export class NabVarComponent {
+
+}
